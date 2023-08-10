b3cc2872a5f96d0f54fad51e4f5eaf2a5fa91461
FullMetalFist <m.vilabrera@gmail.com>
2023-08-10 12:28:58 -0400

    Displaying the Sidebar

--- Recipes/SceneDelegate.swift
+++ Recipes/SceneDelegate.swift
@@ -8,15 +8,22 @@ The main scene delegate.
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
     var splitViewDelegate = SplitViewDelegate()
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
+        if #available(iOS 14, *) {
+            if window?.traitCollection.userInterfaceIdiom == .pad {
+                if let splitViewController = createThreeColumnSplitViewController() {
+                    window?.rootViewController = splitViewController
+                }
+            }
+        }
         configureSplitViewController()
     }
-
+    
     private func configureSplitViewController() {
         guard
             let window = window,
@@ -24,9 +31,27 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         else {
             fatalError()
         }
-
         splitViewController.delegate = splitViewDelegate
     }
-
 }
 
+@available(iOS 14, *)
+extension SceneDelegate {
+    
+    private func createThreeColumnSplitViewController() -> UISplitViewController? {
+        guard let recipeListViewController = RecipeListViewController.instantiateFromStoryboard(),
+              let recipeDetailViewController = RecipeDetailViewController.instantiateFromStoryboard() else { return nil }
+        
+        let sidebarViewController = SidebarViewController()
+        
+        let splitViewController = UISplitViewController(style: .tripleColumn)
+        splitViewController.primaryBackgroundStyle = .sidebar
+        splitViewController.preferredDisplayMode = .twoBesideSecondary
+        
+        splitViewController.setViewController(sidebarViewController, for: .primary)
+        splitViewController.setViewController(recipeListViewController, for: .supplementary)
+        splitViewController.setViewController(recipeDetailViewController, for: .secondary)
+        
+        return splitViewController
+    }
+}
